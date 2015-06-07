5a3b917d4bbfe1d7890af962ee81469562efdaa6
Spencer Creasey <spence@users.noreply.github.com>
2015-06-07 02:23:44 -0400

    checking in useless secrets :/

---
+++ env.sh
@@ -0,0 +1,8 @@
+#!/bin/sh -e
+
+export GAUTH_CLIENT_ID="867427487654-4o9ugpqmst50dscliec6d95mokh0k2j1.apps.googleusercontent.com"
+
+export SECRET_KEY="rWpEAVAWVQnY{7=@D72TRwgJJNiK"
+
+export MYSQL_USER="slackuser"
+export MYSQL_PASS="Y4LU3ZxdMD8aL"
